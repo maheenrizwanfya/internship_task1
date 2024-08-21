f14f32f6752bdd2d6322a5c00eeb73cdaf1b2af3
maheenrizwanfya <maheenrizwanfya@gmail.com>
2024-08-21 18:18:04 +0500

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    String name;
+    double grade;
+
+    public Student(String name, double grade){
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+}
